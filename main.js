23857c99c9d7a3314b0e56f6300f8acf7c0b9f23
Muhammad Kamal <mkamalkayani@gmail.com>
2018-02-17 12:48:07 +0100

    added random number

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ var quoteText = document.querySelector('section blockquote p');
 var newQuote = document.querySelector("button.newQuote");
 var HEIGHT = window.innerHeight - 200;
 
+var category = ["Love", "Patience", "Hope", "Good Character"];
+
+var min = 0;
+var max = 3;
+var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
 URL = "motivationalMoments.json";
 
 newQuote.onclick = function() {
@@ -9,9 +15,9 @@ newQuote.onclick = function() {
  request.open('GET',URL);
  request.send();
 
- request.onload = function randomJson(){
- 	var responseText = JSON.parse(request.responseText);
- 	quoteText.textContent = responseText.contents.quotes[0].quote;
+ request.onload = function (){
+ 	var responseText = request.responseText;
+ 	quoteText.textContent = responseText.Love[0].quote;
  	//console.log(response);
  }
 };
