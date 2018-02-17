6197722aeaaa15722b34b06102abe213569ee8a1
Muhammad Kamal <mkamalkayani@gmail.com>
2018-02-17 12:24:29 +0100

    "Update all changes"

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-newQuote = document.getElementsByClassName("newQuote");
+var quoteText = document.querySelector('section blockquote p');
+var newQuote = document.querySelector("button.newQuote");
+var HEIGHT = window.innerHeight - 200;
+
+URL = "motivationalMoments.json";
+
+newQuote.onclick = function() {
+ var request = new XMLHttpRequest();
+ request.open('GET',URL);
+ request.send();
+
+ request.onload = function randomJson(){
+ 	var responseText = JSON.parse(request.responseText);
+ 	quoteText.textContent = responseText.contents.quotes[0].quote;
+ 	//console.log(response);
+ }
+};
+
+
+
