6177e7506f27354f89649c996cf811cc03b2142f
Muhammad Kamal <mkamalkayani@gmail.com>
2018-02-17 20:24:40 +0100

    XHR working

--- main.js
+++ main.js
@@ -1,26 +1,36 @@
 var quoteText = document.querySelector('section blockquote p');
+var quoteAuthor = document.querySelector('.blockquote-footer')
 var newQuote = document.querySelector("button.newQuote");
 var HEIGHT = window.innerHeight - 200;
 
 var category = ["Love", "Patience", "Hope", "Good Character"];
-
-var min = 0;
-var max = 3;
-var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+function randomNumber(min=0, max=3){
+		var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+		return randomNumber;
+ 	}
 
 URL = "motivationalMoments.json";
 
-newQuote.onclick = function() {
+function newRequest() {
  var request = new XMLHttpRequest();
  request.open('GET',URL);
  request.send();
 
  request.onload = function (){
- 	var responseText = request.responseText;
- 	//quoteText.textContent = responseText.Love[0].quote;
- 	console.log(request);
+ 	var responseText = JSON.parse(request.responseText);
+
+ 	var randomCategory = responseText[category[randomNumber()]];
+
+ 	var randomQuote = randomCategory[randomNumber(min=0, max=randomCategory.length-1)];
+ 	//console.log(randomQuote.author);
+ 	quoteText.textContent = randomQuote.quote;
+ 	quoteAuthor.textContent = randomQuote.author;
  }
 };
+
+window.onload = newRequest();
+newQuote.onclick = function() { newRequest() };
+
 
 
 
